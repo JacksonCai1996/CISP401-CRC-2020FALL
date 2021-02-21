eb1db94ce36eea5a7f3ffd3f5222d21f522c451a
JacksonCai1996 <76241176+JacksonCai1996@users.noreply.github.com>
2021-02-20 22:53:21 -0800

    Add files via upload

---
+++ A3/Multiply3Test.java
@@ -0,0 +1,9 @@
+public class Multiply3Test {
+    public static void main(String[] args)
+    {
+        Multiplay3 object = new Multiplay3();
+
+        object.quiz();
+        object.printQuestionResult();
+    }
+}
